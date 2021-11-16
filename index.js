7b618188823bafc1e141ba3dc4d156041170c208
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-11-15 23:06:56 -0300

    #2 Implemented Put, Get, Post e Delete

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+app.delete('/products/:id', (req, res) => {
+  res.send({
+    success: true,
+    data: 'id ' + req.params.id
+  })
+})
+
+app.patch('/products/:id', (req, res) => {
+  console.log(req.body)
+  res.send({
+    success: true,
+    data: 'id ' + req.params.id
+  })
+})
+
+app.put('/products/:id', (req, res) => {
+  console.log(req.body)
+  res.send({
+    success: true,
+    data: 'id ' + req.params.id
+  })
+})
+
+app.post('/products', (req, res) => {
+  console.log(req.body)
+  res.send({
+    success: true,
+    data: req.body
+  })
+})
+
+app.get('/products/:id', (req, res) => {
+  res.send({
+    name: 'Product ' + req.params.id
+  })
+})
 
 app.get('/products', (req, res) => {
   res.send({
