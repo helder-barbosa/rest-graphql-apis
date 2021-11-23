06c688a74b4bae6d65599c4405345ec036ab3f00
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-11-23 19:05:30 -0300

    #4 GraphQL Basics

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const routes = require('./routes/index')
+const { ApolloServer, gql } = require('apollo-server-express')
 
+// rest
 app.use(bodyParser.json())
-
 app.use(routes)
+
+// graphql
+
+const typeDefs = gql`
+  type Query {
+    getAllProducts : String
+  }
+`
+const resolvers = {
+  Query: {
+    getAllProducts: () => 'All products'
+  }
+}
+
+const graphqlServer = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+graphqlServer.applyMiddleware({ app })
 
 
 app.listen(3000, (err) => {
