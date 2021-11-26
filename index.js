7d5a7573a2cab5cd66c6bc3f0ac9773a681e8b99
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-11-26 19:24:42 -0300

    #6 Organize GraphQL

--- index.js
+++ index.js
@@ -2,49 +2,11 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const routes = require('./routes/index')
-const { ApolloServer, gql } = require('apollo-server-express')
+const graphqlServer = require('./graphql')
 
 // rest
 app.use(bodyParser.json())
 app.use(routes)
-
-// graphql
-
-const typeDefs = gql`
-  type Query {
-    getAllProducts : String
-  }
-  type Product {
-    id: String
-    name: String
-  }
-  type Mutation {
-    createProduct(input: ProductInput): Product
-  }
-  input ProductInput{
-    id: String!
-    name: String!
-  }
-`
-const resolvers = {
-  Query: {
-    getAllProducts: () => [{ id: '1', name: 'All products' }]
-  },
-  Mutation: {
-    createProduct: (context, { input }) => {
-      const { id, name } = input
-      console.log(id, name);
-      return {
-        id, name
-      }
-    }
-  }
-}
-
-const graphqlServer = new ApolloServer({
-  typeDefs,
-  resolvers
-})
 
 graphqlServer.applyMiddleware({ app })
 
