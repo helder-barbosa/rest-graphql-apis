0204ad7a57e2978467624f7f353047b0142793ee
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-12-04 22:15:32 -0300

    #12 GraphQL: update e delete product

--- graphql/resolvers/products.js
+++ graphql/resolvers/products.js
@@ -1,5 +1,6 @@
 const db = require('../../db')
 const Product = require('../../models/product')(db)
+const { ApolloError } = require('apollo-server-express')
 
 const getAllProducts = async (context, { filter }) => {
   let products = null
@@ -19,7 +20,36 @@ const createProduct = async (context, { input }) => {
   }
 }
 
+const deleteProduct = async (context, { id }) => {
+  await Product.remove(id)
+  return true
+}
+
+const updateProduct = async (context, { id, input }) => {
+  const oldProduct = await Product.findByID(id)
+  if (!oldProduct) {
+    throw new ApolloError('Product not found')
+  }
+  if (input.product) {
+    oldProduct.product = input.product
+  }
+  if (input.price) {
+    oldProduct.price = input.price
+  }
+  await Product.update(id, [oldProduct.product, oldProduct.price])
+
+  if (input.categories) {
+    try {
+      await Product.updateCategories(id, input.categories)
+    } catch (err) {
+      throw new ApolloError('Category not found')
+    }
+  }
+
+  return oldProduct
+}
+
 module.exports = {
-  getAllProducts, createProduct
+  getAllProducts, createProduct, deleteProduct, updateProduct
 }
 
