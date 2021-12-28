83ff50e9b709c9f1e564e4b16f91ed538892869e
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-12-27 23:06:25 -0300

    #17 JWT - HoC e auth no GraphQL

--- graphql/index.js
+++ graphql/index.js
@@ -3,13 +3,27 @@ const fs = require('fs')
 const path = require('path')
 const schema = fs.readFileSync(path.join(__dirname, 'schema.graphql'))
 const resolvers = require('./resolvers')
+const jwt = require('jsonwebtoken')
 
 const typeDefs = gql`${schema}`
 
 
 const graphqlServer = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: ({ req }) => {
+    if (req.headers && req.headers.authorization) {
+      const header = req.headers.authorization
+      const headerParts = header.split(' ')
+      const secret = 'asdfghjkl'
+      try {
+        const payload = jwt.verify(headerParts[1], secret)
+        return { user: payload.user }
+      } catch (err) { }
+    }
+    return {
+    }
+  }
 })
 
 
